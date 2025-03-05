0d53cd833500a0fe288c32d3c8fa3bf41bf1d249
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-03-05 10:09:15 -0600

    Add project files.

---
+++ Intergalactic_Race_League.DAL/IrlDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Intergalactic_Race_League.Models;
+using Microsoft.Identity.Client;
+
+namespace Intergalactic_Race_League.DAL
+{
+    public class IrlDbContext : DbContext
+    {
+        public IrlDbContext(DbContextOptions<IrlDbContext> options) : base(options) {   }
+        public DbSet<Racer> Racers { get; set; }
+        public DbSet<Tournament> Tournaments { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ Intergalactic_Race_League/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intergalactic_Race_League.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
