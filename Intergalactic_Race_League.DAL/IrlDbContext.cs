cba2800e1c5dc5f627f1654229db00402162ab36
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-03-08 15:01:12 -0600

    Save Point

--- Intergalactic_Race_League.DAL/IrlDbContext.cs
+++ Intergalactic_Race_League.DAL/IrlDbContext.cs
@@ -15,6 +15,17 @@ namespace Intergalactic_Race_League.DAL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //  Primary Keys
+            modelBuilder.Entity<Vehicle>()
+                .HasKey(v => v.VehicleId);
+            modelBuilder.Entity<Racer>()
+                .HasKey(r => r.RacerId);
+            modelBuilder.Entity<RacerVehicle>()
+                .HasKey(rv => rv.RacerVehicleId);
+            modelBuilder.Entity<Race>()
+                .HasKey(r => r.RaceId);
+            modelBuilder.Entity<Tournament>()
+                .HasKey(r => r.TournamentId);
             //  Relationships
             //  One-to-many: Tournament <-> Race (starting and finishing)
             modelBuilder.Entity<Tournament>()
@@ -44,6 +55,46 @@ namespace Intergalactic_Race_League.DAL
                 .WithOne(rv => rv.Racer)
                 .HasForeignKey<RacerVehicle>(rv => rv.RacerId)
                 .OnDelete(DeleteBehavior.Cascade);
+            //  Property Contraints
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.Model)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.Type)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Racer>()
+                .Property(r => r.DriverName)
+                .IsRequired()
+                .HasMaxLength(100);
+            modelBuilder.Entity<Racer>()
+                .Property(r => r.DriverAge);
+            modelBuilder.Entity<Racer>()
+                .Property(r => r.DriverHeightInCm)
+                .IsRequired();
+            modelBuilder.Entity<Racer>()
+                .Property(r => r.DriverCountry)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Race>()
+                .Property(r => r.RaceName)
+                .IsRequired();
+            modelBuilder.Entity<Race>()
+                .Property(r => r.Status)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Race>()
+                .Property(r => r.TimeStamp);
+            modelBuilder.Entity<Race>()
+                .Property(r => r.Winner);
+            modelBuilder.Entity<Tournament>()
+                .Property(t => t.Status)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Tournament>()
+                .Property(t => t.TimeStamp);
+            // The Lists?? 
         }
     }
 }
